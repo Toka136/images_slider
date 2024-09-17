39d0082668429bc6102988a9a3114391d7441bd6
Toka136 <145512601+Toka136@users.noreply.github.com>
2024-09-17 14:30:23 -0900

    silder
    slider

---
+++ main.js
@@ -0,0 +1,120 @@
+let img_container=document.getElementById("img-container");
+let container=document.getElementById("container");
+let pervious=document.getElementById("pervious");
+let next=document.getElementById("next");
+let images=document.querySelectorAll(".img img")
+let buttons =document.querySelectorAll(".but i");
+function change(i)
+{
+   images.forEach((elem)=>
+    {
+        elem.classList.add("hidden");
+    })
+    images[i].classList.remove("hidden");
+    for(let j=0;j<buttons.length;j++)
+    {
+        if(buttons[j].classList.contains("active")){
+            buttons[j].classList.remove("active");
+            break;
+        }
+    }
+    buttons[i].classList.add("active");
+    count=i;
+}
+function active(j)
+{
+    for(let i=0;i<buttons.length;i++)
+        {
+            if(buttons[i].classList.contains("active")){
+                buttons[i].classList.remove("active");
+                break;
+            }
+        }
+        if(f)
+         {
+        buttons[j].classList.add("active");
+        f=1;
+         }
+         else
+         buttons[j].classList.add("active");
+}
+let count=0,f=0,f2=0;
+let int=setInterval(slide, 5000);
+function slide()
+{
+    count++;
+    if(count>=4)
+        {
+            
+            images[count-1].classList.add("hidden");
+            count=0;
+        }
+        else
+        {
+            images[count-1].classList.add("hidden");
+        }
+        images[count].classList.remove("hidden");
+        images[count].classList.add("animi");
+
+        active(count);
+        
+
+}
+
+next.onclick=function()
+{   
+     count++;
+    if(count>=4)
+    {
+       
+        images[count-1].classList.add("hidden");
+         count=0;
+    }
+    else
+    {
+        images[count-1].classList.add("hidden");
+    }
+    images[count].classList.remove("hidden");
+    images[count].classList.add("animi");
+
+    active(count);
+    
+}
+pervious.onclick=function()
+{
+   
+       count--;
+        if(count<0)
+        {
+            images[0].classList.add("hidden");
+            count=3;
+        }
+        
+        else
+        {
+            // images[count+1].classList.add("animi");
+            images[count+1].classList.add("hidden");
+        }
+        active(count);
+        
+        images[count].classList.remove("hidden");
+        images[count].classList.add("animi");
+   
+    
+}
+
+
+container.onmouseover =function ()
+{
+    console.log("in");
+    clearInterval(int);
+    f2=0;
+}
+container.onmouseout=function()
+{
+    if(!f2)
+    {
+        f2=1;
+    int=setInterval(slide, 5000);
+    }
+}
